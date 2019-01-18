011dc39f543a6beb54ab4b8937368f02bd3979ec
welldoer <welldoer@263.net>
2019-01-18 16:17:47 +0800

    :white_check_mark: 增强测试
    1）对请求信息进行构造，显示处理结果

--- src/test/java/net/blogjava/tddspringboot/PostControllerIntegrationTest.java
+++ src/test/java/net/blogjava/tddspringboot/PostControllerIntegrationTest.java
@@ -2,13 +2,18 @@ package net.blogjava.tddspringboot;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,8 +24,16 @@ public class PostControllerIntegrationTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void test() {
+	public void testSentValidPostRequest() throws Exception {
 		assertThat( mockMvc ).isNotNull();
+		
+		ResultActions resultActions = mockMvc.perform(
+				post("/v2/posts")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{}")
+				);
+		resultActions.andDo(print());
+		assertThat( resultActions ).isNotNull();
 	}
 
 }
